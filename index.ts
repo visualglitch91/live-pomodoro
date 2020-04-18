4c2271e1eca2b0fa1cfe438015b5b4de6663c276
visualglitch91 <visualglitch91@protonmail.com>
2020-04-18 04:29:22 -0300

    Add a button to pause and resume the timer

--- index.ts
+++ index.ts
@@ -1,25 +1,52 @@
-import pomodoro, { Phase, State } from "./pomodoro";
+import pomodoro, { Phase, State as PomodoroState } from "./pomodoro";
+import running, { State as RunningState } from "./running";
+
+interface State {
+  running: RunningState;
+  pomodoro: PomodoroState;
+}
 
 function main() {
   const timerNode = document.getElementById("timer");
   const phaseNode = document.getElementById("phase");
+  const toggleRunningButton = document.getElementById("toggle-running");
 
   let state: State = {
-    timer: 0,
-    phase: Phase.WORK,
-    workCount: 0,
+    running: true,
+    pomodoro: {
+      timer: 0,
+      phase: Phase.WORK,
+      workCount: 0,
+    },
   };
 
   function render() {
-    timerNode.textContent = state.timer.toString();
-    phaseNode.textContent = state.phase;
+    timerNode.textContent = state.pomodoro.timer.toString();
+    phaseNode.textContent = state.pomodoro.phase;
+    toggleRunningButton.textContent = state.running ? "Pause" : "Resume";
   }
+
+  function updateStateAndRender(nextState: State) {
+    state = nextState;
+    render();
+  }
+
+  toggleRunningButton.onclick = () => {
+    updateStateAndRender({
+      ...state,
+      running: running(state.running),
+    });
+  };
 
   render();
 
   setInterval(() => {
-    state = pomodoro(state);
-    render();
+    if (state.running) {
+      updateStateAndRender({
+        ...state,
+        pomodoro: pomodoro(state.pomodoro),
+      });
+    }
   }, 1000);
 }
 
