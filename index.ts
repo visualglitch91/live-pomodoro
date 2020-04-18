97c66f8a40f264b95915ebde7039ef58fbb49595
visualglitch91 <visualglitch91@protonmail.com>
2020-04-18 04:17:10 -0300

    Render an interface with the pomodoro state

--- index.ts
+++ index.ts
@@ -1,15 +1,25 @@
 import pomodoro, { Phase, State } from "./pomodoro";
 
 function main() {
+  const timerNode = document.getElementById("timer");
+  const phaseNode = document.getElementById("phase");
+
   let state: State = {
     timer: 0,
     phase: Phase.WORK,
     workCount: 0,
   };
 
+  function render() {
+    timerNode.textContent = state.timer.toString();
+    phaseNode.textContent = state.phase;
+  }
+
+  render();
+
   setInterval(() => {
     state = pomodoro(state);
-    console.log(state);
+    render();
   }, 1000);
 }
 
